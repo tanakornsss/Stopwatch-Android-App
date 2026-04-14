97754860b7c830b29fd7984b8ebc218d92593688
tanakornsss <omtanakorn2550@gmail.com>
2026-04-14 10:29:26 +0700

    Upgrade plugins

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,13 +1,13 @@
+import com.android.build.api.dsl.ApplicationExtension
+
 plugins {
     id("com.android.application")
-    id("org.jetbrains.kotlin.android")
-    id("kotlin-kapt")
-    id("dagger.hilt.android.plugin")
+    id("org.jetbrains.kotlin.plugin.compose")
+//    id("kotlin-kapt")
+//    id("dagger.hilt.android.plugin")
 }
 
-val compose_ui_version: String by project
-
-android {
+extensions.configure<ApplicationExtension> {
     namespace = "com.example.stopwatch"
     compileSdk = 36
 
@@ -36,12 +36,8 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-
-    kotlinOptions {
-        jvmTarget = "1.8"
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     buildFeatures {
@@ -52,7 +48,7 @@ android {
         kotlinCompilerExtensionVersion = "1.2.0"
     }
 
-    packagingOptions {
+    packaging {
         resources {
             excludes += setOf("/META-INF/{AL2.0,LGPL2.1}")
         }
@@ -61,23 +57,23 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.5.1")
-    implementation("androidx.activity:activity-compose:1.6.0")
+    implementation("androidx.core:core-ktx:1.18.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.10.0")
+    implementation("androidx.activity:activity-compose:1.13.0")
 
-    implementation("androidx.compose.ui:ui:$compose_ui_version")
-    implementation("androidx.compose.ui:ui-tooling-preview:$compose_ui_version")
-    implementation("androidx.compose.material:material:1.2.1")
+    implementation("androidx.compose.ui:ui:1.10.6")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.10.6")
+    implementation("androidx.compose.material:material:1.10.6")
 
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.3")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:$compose_ui_version")
+    androidTestImplementation("androidx.test.ext:junit:1.3.0")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.10.6")
 
-    debugImplementation("androidx.compose.ui:ui-tooling:$compose_ui_version")
-    debugImplementation("androidx.compose.ui:ui-test-manifest:$compose_ui_version")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.10.6")
+    debugImplementation("androidx.compose.ui:ui-test-manifest:1.10.6")
 
     // Dagger - Hilt
-    implementation("com.google.dagger:hilt-android:2.43.2")
-    kapt("com.google.dagger:hilt-android-compiler:2.42")
+//    implementation("com.google.dagger:hilt-android:2.59.2")
+//    kapt("com.google.dagger:hilt-android-compiler:2.59.2")
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,13 @@
 buildscript {
-    extra.apply {
-        set("compose_ui_version", "1.2.1")
-    }
-    dependencies {
-        classpath("com.google.dagger:hilt-android-gradle-plugin:2.43.2")
-    }
+//    dependencies {
+//        classpath("com.google.dagger:hilt-android-gradle-plugin:2.59.2")
+//    }
 }
 
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "7.4.2" apply false
-    id("com.android.library") version "7.4.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.7.0" apply false
+    id("com.android.application") version "9.1.1" apply false
+    id("com.android.library") version "9.1.1" apply false
+    id("org.jetbrains.kotlin.android") version "2.3.20" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.3.20" apply false
 }
